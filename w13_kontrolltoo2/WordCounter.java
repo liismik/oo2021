a264652b6a1f69b3dc5d7d448b29aed5344980d5
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-05-07 13:39:22 +0300

    Add files via upload

--- w13_kontrolltoo2/WordCounter.java
+++ w13_kontrolltoo2/WordCounter.java
@@ -1,3 +1,13 @@
+/*
+Salvesta uurimiseks vähemalt paarileheküljeline tekst.
+* Koosta programm, mis loeks kokku, mitu kolmetähelist sõna on tekstis.
+* Koosta graafik, kus ühel teljel on teksti algusest sinnamaani esinenud kolmetäheliste sõnade arv ning teisel teljel 
+  selleks ajaks leitud kolmetäheliste erinevate sõnade arv.
+* Võta uuringuks tunduvalt pikem tekst. Kuva joonisele sarnased graafikud 1-10 tähe pikkuste sõnade kohta. Mummukestega 
+  tähista joonistele kohad, kus on läbitud veerand, pool ja kolmveerand tekstist.
+*/
+
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
