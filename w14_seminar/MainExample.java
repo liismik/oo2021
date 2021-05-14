e93b3a336d1860946e37089343e0dcfa5b8842d8
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-05-14 11:54:17 +0300

    Add files via upload

---
+++ w14_seminar/MainExample.java
@@ -0,0 +1,20 @@
+public class MainExample {
+    public static void main(String[] args){
+
+    //algväärtustamine
+    String[] autod = {"Audi", "Ford", "Dodge", "Toyota"};
+
+    //massiivi neljanda elemendi kuvamine
+    System.out.println(autod[3]);
+
+    //massiivi neljanda elemendi muutmine ja kuvamine
+    autod[3] = "Mazda";
+    System.out.println(autod[3]);
+
+    //kuvab autode massiivi pikkuse
+    System.out.println(autod.length);
+    }
+}
+
+
+
---
+++ w14_seminar/examples.java
@@ -0,0 +1,22 @@
+public static void examples {
+    int[] IntMassiiv;
+    byte[] ByteMassiiv;
+    short[] ShortMassiiv;
+    long[] LongMassiiv;
+    float[] FloatMassiiv;
+    double[] DoubleMassiiv;
+    boolean[] BooleanMassiiv;
+    char[] CharMassiiv;
+    String[] StringMassiiv;
+
+    int[] mingidNumbrid = new int[10];
+
+    int pikkus = mingidNumbrid.length;
+
+    int[] otherNumbers = new int[5];
+
+    int[][] numbrid = new int[6][5];
+}
+
+
+
